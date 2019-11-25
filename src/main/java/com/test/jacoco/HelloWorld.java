2a353b8917bd36c22c22a623ab1d33f0b67f1862
Jumpingrain <1826168699@qq.com>
2019-11-25 15:53:16 +0800

    updata

--- src/main/java/com/test/jacoco/HelloWorld.java
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
     	if((a == b) || ((c == d) && (bug(a)))) {
     		 x = 1; 
     	}
-    	e = 1/x;
+    	e = 1+x;
     	return 0;
     }
     public boolean bug(int a) {
@@ -46,28 +46,49 @@ public class HelloWorld {
     
     
     public boolean isTriangle(int a, int b, int c) {
-    	/**
-    	 * TODO: You need to complete this method to determine whether  a
-    	 * triangle is formed or not when given the input edge a, b and c.
-    	 */
+		if(a<=0||b<=0||c<=0)
+			return false;
+		else if(a+b<=c||a+c<=b||b+c<=a)
+			return false;
     	return true;
     }
     public boolean isBirthday(int year, int month, int day) {
-    	/**
-    	 * TODO: You need to complete this method to determine whether a 
-    	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
-    	 * formed or not when given the input year, month and day.
-    	 */
-    	return true;
-    	
+		//allow year<0, but year 0000 don't exist.
+		if(year==0||month<=0||day<=0)
+			return false;
+    	if(month==2){
+			if(year%4==0&&year%100!=0)
+				if(day<=29)
+					return true;
+				else
+					return false;
+			else
+				if(day<=28)
+					return true;
+				else
+					return false;
+
+		}
+		else{
+			if((month==1||month==3||month==5||month==7||month==8||month==10||month==12)
+			&&day<=31)
+				return true;
+			else if((month==4||month==6||month==9||month==11)
+			&&day<=30)
+				return true;
+			else 
+				return false;
+		}
     }
     public Double miniCalculator(double a, double b, char op) {
-    	/**
-    	 * TODO: You need to complete this method to form a small calculator which 
-    	 * can calculate the formula: "a op b", the op here can be four basic  
-    	 * operation: "+","-","*","/". 
-    	 */
-    	double result = 0.0;
+		double result = 0.0;
+		switch(op){
+			case '+':result=a+b;break;
+			case '-':result=a-b;break;
+			case '*':result=a*b;break;
+			case '/':if (b!=0) result=a/b;else System.out.println("FAULT");break;
+			default: System.out.println("FAULT");
+		}
     	return result;
     }
     
--- src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -26,14 +26,164 @@ public class HelloWorldTest {
     
     @Test
     public void testMethod3() {
-    	/**
-    	 * TODO：finish the test function
-    	 */
     	HelloWorld hw = new HelloWorld();
-    	int a = hw.Method3(6, -1, 1);
-    	
+        int a0 = hw.Method3(6, -1, 1);
+        int a1 = hw.Method3(6, -1, -1);
+        int a2 = hw.Method3(6, 0, 1);
+        int a3 = hw.Method3(6, 0, -1);
+        int a4 = hw.Method3(4, -1, 1);
+        int a5 = hw.Method3(4, -1, -1);
+        int a6 = hw.Method3(6, -1, -1);
+        int a7 = hw.Method3(4, -1, -3);
+        int a8 = hw.Method3(6, -1, -3);
+        int a9 = hw.Method3(4, -1, -1);
+        assertEquals(a0, 0);
+        assertEquals(a1, 0);
+        assertEquals(a2, 0);
+        assertEquals(a3, 0);
+        assertEquals(a4, 0);
+        assertEquals(a5, 0);
+        assertEquals(a6, 0);
+        assertEquals(a7, 0);
+        assertEquals(a8, 0);
+        assertEquals(a9, 0);
     }
-    /**
-     * TODO: add the test function of other methods in HelloWorld.java
-     */
+
+    @Test
+    public void testMethod4() {
+        HelloWorld hw = new HelloWorld();
+        int a0 = hw.Method4(0,1,1,1,(float)1.0);
+        int a1 = hw.Method4(1,1,1,1,(float)1.0);
+        int a2 = hw.Method4(1,1,1,2,(float)5.0);
+        int a3 = hw.Method4(5,5,1,1,(float)5.0);
+        int a4 = hw.Method4(5,5,1,2,(float)5.0);
+        int a5 = hw.Method4(5,1,1,1,(float)5.0);
+        assertEquals(a0, 0);
+        assertEquals(a1, 0);
+        assertEquals(a2, 0);
+        assertEquals(a3, 0);
+        assertEquals(a4, 0);
+        assertEquals(a5, 0);
+        //int a6 = hw.Method4(5,1,1,2,(float)5.0);
+        //int a7 = hw.Method4(2,1,1,1,(float)5.0);
+        //int a8 = hw.Method4(2,1,1,2,(float)5.0);
+    }
+    
+    @Test
+    public void testbug() {
+        HelloWorld hw = new HelloWorld();
+        boolean a0 = hw.bug(1);
+        assertEquals(a0, false);
+        boolean a1 = hw.bug(5);
+        assertEquals(a1, true);
+    }
+
+    @Test
+    public void testisTriangle() {
+        HelloWorld hw = new HelloWorld();
+        boolean a0 = hw.isTriangle(-1,-1,-1);
+        boolean a1 = hw.isTriangle(1,-1,-1);
+        boolean a2 = hw.isTriangle(1, 1,-1);
+        boolean a3 = hw.isTriangle(2,1,1);
+        boolean a4 = hw.isTriangle(1,2,1);
+        boolean a5 = hw.isTriangle(1,1,2);
+        assertEquals(a0, false);
+        assertEquals(a1, false);
+        assertEquals(a2, false);
+        assertEquals(a3, false);
+        assertEquals(a4, false);
+        assertEquals(a5, false);
+        boolean a6 = hw.isTriangle(2,2,1);
+        assertEquals(a6, true);
+    }
+
+    @Test
+    public void testisBirthday() {
+        HelloWorld hw = new HelloWorld();
+        boolean a;
+        a = hw.isBirthday(0,1,1);
+        assertEquals(a, false);
+        a = hw.isBirthday(1,-1,1);
+        assertEquals(a, false);
+        a = hw.isBirthday(1,1,-1);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,2,28);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,2,29);
+        assertEquals(a, false);
+        a = hw.isBirthday(2004,2,29);
+        assertEquals(a, true);
+        a = hw.isBirthday(2004,2,30);
+        assertEquals(a, false);
+        a = hw.isBirthday(2000,2,28);
+        assertEquals(a, true);
+        a = hw.isBirthday(2000,2,29);
+        assertEquals(a, false);
+
+        a = hw.isBirthday(1999,1,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,1,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,3,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,3,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,5,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,5,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,7,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,7,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,8,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,8,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,10,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,10,32);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,12,31);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,12,32);
+        assertEquals(a, false);
+        
+        a = hw.isBirthday(1999,4,30);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,4,31);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,6,30);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,6,31);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,9,30);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,9,31);
+        assertEquals(a, false);
+        a = hw.isBirthday(1999,11,30);
+        assertEquals(a, true);
+        a = hw.isBirthday(1999,11,31);
+        assertEquals(a, false);
+    }
+
+    @Test
+    public void testmimiCalculator() {
+        HelloWorld hw = new HelloWorld();
+        double a;
+        a = hw.miniCalculator(1.0,3.0,'+');
+        assertEquals(a, 4.0,0.001);
+        a = hw.miniCalculator(1.0,3.0,'-');
+        assertEquals(a, -2.0,0.001);
+        a = hw.miniCalculator(1.0,3.0,'*');
+        assertEquals(a, 3.0,0.001);
+        a = hw.miniCalculator(1.0,3.0,'/');
+        assertEquals(a, 1.0/3,0.001);
+        a = hw.miniCalculator(1.0,0.0,'/');
+        assertEquals(a, 0.0,0.001);
+        a = hw.miniCalculator(1.0,5.0,'(');
+        assertEquals(a, 0.0,0.001);
+    }
 }
+
+    
